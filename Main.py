e14a24ab42babe52df2ffe59ec33b0fe016a022d
JelleWierenga <116724275+JelleWierenga@users.noreply.github.com>
2024-01-30 21:08:53 +0100

    Add Instagram to Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from Social_Media_Username_Finder.Roblox import Roblox
+from Social_Media_Username_Finder.Instagram import Instagram
 
 username = str(input("Enter a username: "))
 Roblox.get_Roblox_Username(username)
+Instagram.get_Instagram_Username(username)
