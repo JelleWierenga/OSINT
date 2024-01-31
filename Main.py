3802c280ac14f8d21e0c6fd5fed8eed1c747b201
JelleWierenga <116724275+JelleWierenga@users.noreply.github.com>
2024-01-31 20:27:00 +0100

    Add x to Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 from Social_Media_Username_Finder.Roblox import Roblox
 from Social_Media_Username_Finder.Instagram import Instagram
+from Social_Media_Username_Finder.X import X
 
 username = str(input("Enter a username: "))
 Roblox.get_Roblox_Username(username)
 Instagram.get_Instagram_Username(username)
+X.get_X_Username(username)
+
+# ToDo
+# - Add option to choose whice category to search in
+# - Make better folder structure
